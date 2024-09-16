77e4c47d109fe5546b81a7ccb55151df5c0c07c2
duongbui <duongbuibd@gmail.com>
2024-09-16 23:46:36 +0700

    Remove AuthLayout component for authentication pages

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -3,6 +3,11 @@
 import React from "react";
 
 const Register = () => {
+  const throwNewError = () => {
+    throw new Error("This is a new error");
+  };
+
+  throwNewError();
   return <div>Register page</div>;
 };
 
